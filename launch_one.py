086b9efc2bfafdb0363a661430ef0fb3147f60a6
Emmanuel BAGOT <bagot.emmanuel@gmail.com>
2023-04-06 13:18:30 +0200

    Launch One

--- launch_one.py
+++ launch_one.py
@@ -4,6 +4,13 @@ import ftplib
 from scrap import Scrap
 from datetime import datetime
 
+import os
+log_file = open('tmp.txt', 'a')
+print(os.popen('whoami').readline(), file = log_file)
+print(os.popen('snscrape').readline(), file = log_file)
+log_file.close()
+exit()
+
 def ftp_is_connected(ftp_host, ftp_login, ftp_pass, ftp_dir):
     if(not ftp_host or not ftp_login or not ftp_pass or not ftp_dir):
         return False
